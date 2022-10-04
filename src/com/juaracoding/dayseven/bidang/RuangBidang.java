acbfa349c3862a823764dc2457f2b21c2db2ace7
Koswara <koswara.c79@gmail.com>
2022-10-04 21:45:45 +0700

    Add OOP

---
+++ src/com/juaracoding/dayseven/bidang/RuangBidang.java
@@ -0,0 +1,13 @@
+package com.juaracoding.dayseven.bidang;
+
+public class RuangBidang {
+
+    String gambar = "default";
+    String namaGambar = "default";
+    
+    public void draw(){
+        System.out.printf(gambar);
+        System.out.printf(namaGambar);
+    }
+
+}
